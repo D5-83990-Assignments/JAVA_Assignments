1132a17f3f6e69c9b0e65a4afcc440eb6a0820a0
D5_83990_Abhishek <162444911+Abhishek-Bhopale@users.noreply.github.com>
2024-04-24 22:54:11 +0530

    Assignment 2 Qu 1

--- Que_01.java
+++ Que_01.java
@@ -1,52 +1,32 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Que_01 {
-	
-	void displayType(String var)
-	{
-		System.out.println("String type");
-	}
-	void displayType(int var)
-	{
-		System.out.println("Integer type");
-	}
-	String displayType(double var)
-	{
-		return "Double";
-	}
-	void displayType(float var)
-	{
-		System.out.println("Float type");
-	}
 
 	public static void main(String[] args) {
-		
-	
-		Que_01 sam = new Que_01();
+		// TODO Auto-generated method stub
+		int allowedLimit;
+		int beginBalance;
+		int charges;
+		int credits;
 		
 		Scanner sc = new Scanner(System.in);
-		double num1 = 0;
-		double num2 = 0;
-		try
+		System.out.println("Enter pending balance at the beginning of the month :- ");
+		beginBalance = sc.nextInt();
+		System.out.println("Enter total of all items charged by the customer this month :- ");
+		charges = sc.nextInt();
+		System.out.println("Enter total of all credits applied to the customer’s account this month :- ");
+		credits = sc.nextInt();
+		System.out.println("Enter allowed credit :- ");
+		allowedLimit = sc.nextInt();
+		int cal = beginBalance + charges - credits;
+		if(allowedLimit < cal)
 		{
-			System.out.print("Enter number 1 :- ");
-			num1 = sc.nextDouble();
-			System.out.print("Enter number 2 :- ");
-			num2 = sc.nextDouble();
-			if(sam.displayType(num1) == "Double" && sam.displayType(num2) == "Double")
-			{
-				System.out.println("Average :- "+(num1+num2)/2);
-			}
-			else
-			{
-				System.out.println("Please enter double value");
-			}
+			System.out.println("Credit limit exceeded");
 		}
-		catch(InputMismatchException e)
+		else
 		{
-			System.out.println("Please enter double value");
+			System.out.println("New balance for next month :- "+cal);
 		}
-		System.out.println("Thanks.....");
 	}
+
 }
