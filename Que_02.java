b12d5a0c226e7cf19828333b3118711b1cbccc6d
D5_83990_Abhishek <162444911+Abhishek-Bhopale@users.noreply.github.com>
2024-05-10 20:00:43 +0530

    Assignment 9 Que 2

--- Que_02.java
+++ Que_02.java
@@ -1,16 +1,25 @@
-import java.util.Scanner;
+package com.sunbeam;
 
-import p1.Qu_02_data;
+import java.util.Scanner;
 
 public class Que_02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Qu_02_data A = new Qu_02_data();
-		A.accept();
-		A.display();
-		
+		String s;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the string :- ");
+		s = sc.next();
+		char[] charS = s.toCharArray();
+		String revS = String.valueOf(charS[s.length()-1]);
+		for(int i=s.length()-2;i>=0;i--)
+		{
+			revS = revS + charS[i]; 
+		}
+		if(revS.equals(s))
+			System.out.println("String is palindrome");
+		else
+			System.out.println("String is not palindrome");
+
 	}
 
 }
