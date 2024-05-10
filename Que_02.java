2b6bb05b0f0459c1e9687a35f86d003e7e410b67
D5_83990_Abhishek <162444911+Abhishek-Bhopale@users.noreply.github.com>
2024-05-10 20:03:04 +0530

    Assignment 10 Que 2

--- Que_02.java
+++ Que_02.java
@@ -1,25 +1,33 @@
 package com.sunbeam;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Que_02 {
 
 	public static void main(String[] args) {
-		String s;
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the string :- ");
-		s = sc.next();
-		char[] charS = s.toCharArray();
-		String revS = String.valueOf(charS[s.length()-1]);
-		for(int i=s.length()-2;i>=0;i--)
-		{
-			revS = revS + charS[i]; 
-		}
-		if(revS.equals(s))
-			System.out.println("String is palindrome");
-		else
-			System.out.println("String is not palindrome");
+		Product[] arr = {
+				new Product(5,"Java Book","Books",540),
+				new Product(3,"Harry Potter","Novel",500),
+				new Product(1,"Choco","Ice Cream",50),
+				new Product(6,"Charger","Electronics",2500),
+				new Product(2,"Sofa","Furniture",25000),
+				new Product(4,"RO","Home Appliance",5000),
+				new Product(10,"Perfume","Cosmetics",250),
+				new Product(7,"Tubelight","Electrical",350),
+				new Product(8,"Tomato","Vegetables",30),
+				new Product(9,"Wheat","Grocery",300),
+		};
+		
+		
+		System.out.println("BEFORE SORTING --->");
+		for(Product ele : arr)
+			System.out.println(ele);
 
+		Arrays.sort(arr);
+		
+		System.out.println("AFTER SORTING --->");
+		for(Product ele : arr)
+			System.out.println(ele);
 	}
 
 }
